fd6cb6854ade89146304fa6ca490f1f167b408ab
Nandita <95146588+NanditaRK@users.noreply.github.com>
2024-10-16 18:07:15 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = ({ aboutRef, eventsRef, contactRef }) => {
           <span className="self-center hidden md:flex text-2xl font-semibold whitespace-nowrap text-gray-900">Silver Creek Mini SO</span>
         </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Register</button>
+          <Link target="_blank" to="https://scilympiad.com/silvercreek"><button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Register</button></Link>
           <button onClick={() => { setMenu(!menu) }} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded={menu}>
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
