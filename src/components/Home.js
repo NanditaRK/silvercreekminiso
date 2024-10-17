560660835e00a1857e3bddcc13f3391d34d359d5
Nandita <95146588+NanditaRK@users.noreply.github.com>
2024-10-16 18:07:52 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,7 +33,7 @@ const Home = ({ onLearnMoreClick }) => {
           <p className=" text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Practice for Science Olympiad with our Mini SO Invitational if you are a Division B team.</p>
           <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"><bold className='font-extrabold'>Registration:</bold> $65.00</p>
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            <a target="_blank" href="https://scilympiad.com/silvercreek" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
               Register
               <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
